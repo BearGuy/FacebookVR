5570fbed54ccc43b7ef8bca2bf6c9965aef62c0c
maddster82 <madd_y@hotmail.com>
2017-03-30 11:24:25 -0400

    Bubble creation

---
+++ VRUnityProject/Assets/LoadForestScene.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadForestScene : MonoBehaviour {
+
+	void OnMouseDown() {
+		Application.LoadLevel ("Forest");
+	}
+}
---
+++ VRUnityProject/Assets/loadHouseScene.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loadHouseScene : MonoBehaviour {
+
+	void OnMouseDown() {
+		Application.LoadLevel ("house_scene");
+	}
+}
